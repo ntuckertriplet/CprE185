6a8b7b1ec83c7835cecc5ac5e141f092ec2d454b
ntuckertriplet <ntuckertriplet@gmail.com>
2019-01-23 16:38:06 -0600

    partial lab1

---
+++ During the Year/lab1/lab1_1.c
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------------------
+-					                    SE 185 Lab 01
+-             Developed for 185-Rursch by T.Tran and K.Wang
+-   Name: Nathan Tucker
+- 	Section:
+-   NetID: njtucker@iastate.edu
+-   Date: 23 January 2019
+-----------------------------------------------------------------------------*/
+
+/*
+  Here is a block comment
+  These lines don't run
+*/
+
+/*-----------------------------------------------------------------------------
+-								                  Includes
+-----------------------------------------------------------------------------*/
+#include <stdio.h>
+
+
+/*-----------------------------------------------------------------------------
+-								                Implementation
+-----------------------------------------------------------------------------*/
+int main()
+{
+
+    // This is a C comment, this line doesn't run in the program
+
+    printf("This is my message\n"); // Modify this line
+    printf("\n"); // This prints a newline character
+
+    return 0; // This is a return statement
+
+}
