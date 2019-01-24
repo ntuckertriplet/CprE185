4c26e75d084c631d8230c7dffb733eb3dcccf6a1
ntuckertriplet <ntuckertriplet@gmail.com>
2019-01-24 15:57:02 -0600

    first class activity

---
+++ During the Year/classActivity.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+ printf("Hello World\n");
+
+ printf("Nate Tucker, Colton Urbaitis, Ian Hatlewick\n");
+
+ printf("I have no more classes, but I have to work at the Memorial Union\n");
+
+ printf("Enter the room that you work: ");
+
+ int roomNumber;
+
+ scanf("%d", &roomNumber);
+
+ printf("The room number is %d\n", roomNumber);
+
+}
