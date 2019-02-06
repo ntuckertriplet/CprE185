6fb4c2b410238cc71e57040125c0f44a726ef27a
ntuckertriplet <ntuckertriplet@gmail.com>
2019-02-06 16:52:54 -0600

    little confusion in the lab

--- During the Year/lab2/lab2_4.c
+++ During the Year/lab2/lab2_4.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+#define M_PI 3.14159265358979323846
 int main(){
-	printf("%d/n", 6427 + 1725);
-	printf("%d/n", (6971 * 3925) – 95);
-	printf("%lf/n", 79 + 12 / 5);
-	printf("%lf/n", 3640.0 / 107.9);
-	printf("%d/n", (22 / 3) * 3);
-	printf("%d/n", 22 / (3 * 3));
-	printf("%lf/n", 22 / (3 * 3));
-	printf("%lf/n", 22 / 3* 3);
-	printf("%lf/n", (22.0 / 3) * 3.0);
+	int a, b, c;
+	double x, y, z;
+	a = 6427;
+	b = 1725;
+	printf("%d + %d = %d/n", a, b, a + b);
+
+	a = 6971;
+	b = 3925;
+	c = 95;
+	printf("(%d * %d) - %d = %d/n", a, b, c, (a * b) – c);
+
+	x = 79;
+	y = 12;
+	z = 5;
+	printf("%lf + %lf / %lf = %lf/n", x, y, z, x + y / z);
+
+	x = 3640.0;
+	y = 107.9;
+	printf("%lf /" %lf = %lf/n", x, y, x / y);
+
+	a = 22;
+	b = 3;
+	printf("(%d /" %d) * %d = %d/n", x, y, y, (x / y) * y);
+
+	printf("%d /" (%d * %d) = %d/n", x, y, y, x / (y * y));
+
+	x = 22;
+	y = 3;
+	printf("%lf /" (%lf * %lf) = %lf/n", x, y, y, x / (y * y));
+
+	printf("%lf /" %lf * %lf = %lf/n", x, y, y, x / y * y);
+
+	x = 22.0;
+	y = 3;
+	z = 3.0;
+	printf("(%lf /" %lf) * %lf/n", x, y, z, (x / y) * z);
+
 	printf("%d/n", 22.0 / (3 * 3.0));
 	printf("%lf/n", 22.0 / 3.0 * 3.0);
 
@@ -22,5 +51,19 @@ int main(){
 	replace r in the first equation: A = π[C/(2π)]2.
 	When we simplify this, we get A = C^2/(4 π).
 	*/
+	double circumference = 23.567;
+	printf("%lf",(circumference * circumference)/(4*M_PI));
+
+	/*
+	Feet to meters is 1 foot for every .3048 meters
+	*/
+	double givenFeet = 14;
+	printf("%lf", givenFeet * .3048);
+
+	/*
+	Given Fahrenheit, convert to Celsius
+	*/
+	double givenF = 76;
+	printf("%lf", (givenF - 32) / 1.8):
 	return 0;
 }
