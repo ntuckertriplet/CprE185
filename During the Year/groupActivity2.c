cdbb15f60ed7d1e7f35c0bb02934ad3b8d5a455d
ntuckertriplet <ntuckertriplet@gmail.com>
2019-02-07 16:43:25 -0600

    stuff

--- During the Year/groupActivity2.c
+++ During the Year/groupActivity2.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//Nathan Tucker, Ian Hatlewick, Colton Urbaitis
+int factorial(int f)
+{
+	if ( f == 0 ){
+        	return 1;
+	} else {
+    		return(f * factorial(f - 1));
+	}
+}
 
 int main() {
 
+	int f;
+	printf("Enter a number greater than 1: ");
+	scanf("%d", &f);
+	printf("%d factorial is: %d\n", f, factorial(f));
 	return 0;
 }
