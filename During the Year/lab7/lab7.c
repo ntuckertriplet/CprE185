33b4692728932244a260986003fb5705246514c2
Nathan Tucker <njtucker@njtucker-mbpro.student.iastate.edu>
2019-03-14 15:02:38 -0500

    lab 7 complete

--- During the Year/lab7/lab7.c
+++ During the Year/lab7/lab7.c
@@ -1,10 +1,10 @@
 /*-----------------------------------------------------------------------------
 -					          SE 185 Lab 07
 -             Developed for 185-Rursch by T.Tran and K.Wang
--	Name: Nathan Tucker
-- 	Section: B
+-	Name: Nate Tucker
+- Section: B
 -	NetID: njtucker
--	Date: 13 March 2019
+-	Date: 27 March 2019
 -
 -   This file provides the outline for your program
 -   Please implement the functions given by the prototypes below and
@@ -24,6 +24,8 @@
 -								Defines
 -----------------------------------------------------------------------------*/
 #define PI 3.141592653589
+#define TRUE 1
+#define FALSE 0
 
 /* NO GLOBAL VARIABLES ALLOWED */
 
@@ -57,7 +59,7 @@ int scaleMagForScreen(double rad);
     This function scales the joystick value to fit on the screen.
     POST: -39 <= return value <= 39
 -----------------------------------------------------------------------------*/
-int scaleJoyForScreen(int rad);
+int scaleJoyForScreen(int* rad);
 
 /*----------------------------------------------------------------------------
     PRE: -39 <= number <= 39
@@ -78,6 +80,9 @@ void print_chars(int num, char use);
 /*-----------------------------------------------------------------------------
 -								Implementation
 -----------------------------------------------------------------------------*/
+
+//./ds4rd.exe -d 054c:05c4 -D DS4_BT -t -g -b -j | ./lab7.exe
+
 int main()
 {
     double x, y, z;                     /* Values of x, y, and z axis*/
@@ -87,23 +92,104 @@ int main()
     int scaled_pitch, scaled_roll; 	    /* Value of the roll/pitch adjusted to fit screen display */
     int scaled_joy;                     /* Value of joystick adjusted to fit screen display */
 
-    /* Put pre-loop preparation code here */
-
+	int button_pushed = 0;
+	int run_mode = 0;
+	
     do
     {
-        /* Scan a line of input */
-
+		read_input(&t, &x, &y, &z, &b_Up, &b_Right, &b_Down, &b_Left, &j_LX, &j_LY, &j_RX, &j_RY);
+		
         /* Calculate and scale for pitch AND roll AND joystick */
 
         /* Switch between roll, pitch, and joystick with the up, down, and right button, respectivly */
-
+		if(b_Left == 1) {
+			button_pushed = 1;
+		} else if (button_pushed && b_Left != 1) {
+			if (run_mode == 2) {
+				state = 0;
+			} else {
+				state++;
+			}
+			button_pushed = 0;
+		}
+		
         /* Output your graph line */
-
-
+		(state == 0) ? print_chars(scaleMagForScreen(x),' ') :
+		(state == 1) ? print_chars(scaleMagForScreen(y),' ') :
+		(state == 2) ? print_chars(scaleJoyForScreen(j_RY),' ') :
+		print_chars(scaleJoyForScreen(j_RX),' ');
+		
         fflush(stdout);
 
-    } while (1 ); /* Modify to stop when left button is pressed */
+    } while (read_input(&t, &x, &y, &z, &b_Up, &b_Right, &b_Down, &b_Left, &j_LX, &j_LY, &j_RX, &j_RY) == FALSE); /* Modify to stop when left button is pressed */
 
     return 0;
 
+}
+
+/*
+This function scans a line of DS4 data, and returns
+True when left button is pressed
+False Otherwise
+*/
+int read_input(int* time, double* g_x, double* g_y, double* g_z, int* button_T, int* button_C, int* button_X, int* button_S, int* l_joy_x, int* l_joy_y, int* r_joy_x, int* r_joy_y ){
+	scanf("%d, %lf, %lf, %lf, %d, %d, %d, %d, %d, %d, %d, %d", &time, &g_x, &g_y, &g_z, &button_T, &button_C, &button_X, &button_S, &l_joy_x, &l_joy_y, &r_joy_y, &r_joy_y);
+	//printf("%d, %lf, %lf, %lf, %d, %d, %d, %d, %d, %d, %d, %d\n", time, g_x, g_y, g_z, button_T, button_C, button_X, button_S, l_joy_x, l_joy_y, r_joy_x, r_joy_y);
+	if(button_S == 0) {
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/*
+This function scales the roll/pitch value to fit on the screen.
+Input should be capped at either -1.0 or 1.0 before the rest of your
+conversion.
+*/
+int scaleMagForScreen(double rad) {
+	return (int)((((asin(rad) + PI /2) / PI) * 78) - 39);
+}
+
+/* This function scales the joystick value to fit on the screen. */
+int scaleJoyForScreen(int joystick_VAL) {
+	return (int)((((joystick_VAL + 128.0) / 255.0) * 78) - 39)
+
+}
+
+/*
+Uses print_chars to graph a number from -39 to 39 on the screen.
+You may assume that the screen is 80 characters wide.
+*/
+void graph_line(int number) {
+  /* Apparently I made print_chars way too broken, this method is unneeded for the time being */
+}
+
+/*
+This function prints the character "use" to the screen "num" times
+This function is the ONLY place printf is allowed to be used
+*/
+void print_chars(int num, char use) {
+  if(num > 0){
+		for(int j = 0; j < 39; j++){
+			printf(" ");
+		}
+		for(int j = 0; j < num; j++){
+			printf("r");
+		}
+	}
+	else if(num < 0){
+		for(int j = 0; j < 39 + num; j++){
+			printf(" ");
+		}
+		for(int j = 39 + num; j < 40; j++){
+			printf("l");
+		}
+	}
+	else{
+		for(int j = 0; j < 39; j++){
+			printf(" ");
+		}
+		printf("0");
+	}
+	printf("\n");
 }
