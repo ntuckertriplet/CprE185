3c42be663f553d90b9d6e0ee2405c1f1b6900bdd
Nate Tucker <ntuckertriplet@gmail.com>
2019-05-01 17:49:08 -0600

    final update

--- During the Year/RSA/rsa.c
+++ During the Year/RSA/rsa.c
@@ -25,6 +25,7 @@ struct private_key_struct{
 };
 
 
+
 // Recursive function to return gcd of a and b 
 int gcd(int a, int b) 
 { 
@@ -46,59 +47,70 @@ int gcd(int a, int b)
     return gcd(a, b-a); 
 }
 
-
+// this function is using the extended euclidian algorithm to calculate d
+// essentially, to find d, it needs to satisfy de ≡ 1 mod phi
+// it's a little hard to follow, and even some of the logic goes a little over my head
+// all i know it that it works, and if it works, it doesn't need fixed
 long long calculate_d(long long a, long long b){
- long long x = 0, d = 1, u = 1, v = 0, gcd = b, m, n, q, r;
- while (a!=0) {
-   q = gcd/a; r = gcd % a;
-   m = x-u*q; n = d-v*q;
-   gcd = a; a = r; x = u; d = v; u = m; v = n;
-   }
+ long long x = 0, d = 1, u = 1, v = 0; 
+ long long gcd = b, var_a, n, q, var_b;
+ do{
+   q = gcd/a; 
+   var_b = gcd % a;
+   var_a = x-u*q; 
+   n = d-v*q;
+   gcd = a; 
+   a = var_b; 
+   x = u; 
+   d = v; 
+   u = var_a; 
+   v = n;
+   } while (a != 0); 
    return d;
 }
 
 // recursively go through and go from encrypted to decrypted or vice versa
 // the math works the same when given the public key to encrypt and the private key to decrypt
-long long calculate_mod_exp(long long mess, long long e, long long m){
+long long calculate_mod_exp(long long mess, long long exp, long long mod){
 // if any are negative or the message is less than or equal to zero, it won't work, break
-  if (mess < 0 || e < 0 || m <= 0){
-    exit(1);
+  if (mess < 0 || exp < 0 || mod <= 0){
+    exit(-1);
   }
-  mess = mess % m;
-  if(e == 0) return 1; // x to the 0 is 1
-  if(e == 1) return b; // x to the 1 in x
+  mess %= mod;
+  
   //case if even
-  if(e % 2 == 0){
-    return (calculate_mod_exp(mess * mess % m, e/2, m) % m);
+  if(exp % 2 == 0){
+    return (calculate_mod_exp(mess * mess % mod), (exp / 2), mod) % mod);
   }
   //case if odd
   if(e % 2 == 1){
-    return (mess * calculate_mod_exp(mess, (e-1), m) % m);
+    return (mess * calculate_mod_exp(mess, (exp - 1), mod) % mod);
   }
 
 }
 
 
 
-//a buffer as 2 ^ 10 size
-char buffer[1024];
+//a buffer of a large size with more than enough spaces for the prime numbers
+//a null character will be appended in the correct position at the end of the primes list
+char buffer[1000];
 
 //unchanged value, ergo const
-const int MAX_DIGITS = 50;
+const int largest_nums = 64;
 
 
 // generate a public and private key, given the primes list and the key structs
 void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv, char *primelistfile)
 {
-  FILE *primes_list;
+  FILE *prime_list = fopen(primelistfile, "r");
 
   // count number of primes in the list
   int num_primes = 0;
  
   //do - while because it will need to be done at least once
   do{
-    //read in the file
-    int position = fread(buffer,1,sizeof(buffer)-1, primes_list);
+    //read in the file, putting each element in the buffer
+    int position = fread(buffer, 1, sizeof(buffer) - 1, prime_list);
    
     //need a null character to stop an array
     buffer[position] = '\0';
@@ -107,7 +119,7 @@ void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv
       //file is in a list format, with one number on each line.
       //therefore, every number would require a new line space except the last one
       if (buffer[i] == '\n'){
-	num_primes++;
+		num_primes++;
       }
     }
   }
@@ -124,7 +136,7 @@ void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv
   //powl is pow using longs
   long long e = powl(2, 8) + 1; // e = 257
   long long d = 0;
-  char prime_buffer[MAX_DIGITS];
+  char prime_buffer[largest_nums];
   long long max = 0;
   long long totient_n = 0;
   
@@ -146,7 +158,7 @@ void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv
     // here we find the prime at position b, store it as q
     rewind(primes_list);
     for(i=0; i < b + 1; i++){
-      for(j=0; j < MAX_DIGITS; j++){
+      for(j=0; j < largest_nums; j++){
 	prime_buffer[j] = 0;
       }
       fgets(prime_buffer,sizeof(prime_buffer)-1, primes_list);
@@ -176,12 +188,9 @@ void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv
 }
 
 
-long long *rsa_encrypt(const char *message, const unsigned long message_size, const struct public_key_struct *pub){
+long long *rsa_encrypt(char *message, unsigned long message_size, struct public_key_struct *pub){
   long long *encrypted = malloc(sizeof(long long)*message_size);
-  if(encrypted == NULL){
-    fprintf(stderr, "Error in encryption.\n");
-    return NULL;
-  }
+  
   long long i = 0;
   for(i=0; i < message_size; i++){
     encrypted[i] = calculate_mod_exp(message[i], pub->exponent, pub->modulus);
@@ -190,18 +199,11 @@ long long *rsa_encrypt(const char *message, const unsigned long message_size, co
 }
 
 
-char *rsa_decrypt(const long long *message, const unsigned long message_size, const struct private_key_struct *priv){
-  if(message_size % sizeof(long long) != 0){
-    fprintf(stderr,"Error: message_size is not divisible by %d, so cannot be output of rsa_encrypt\n", (int)sizeof(long long));
-     return NULL;
-  }
+char *rsa_decrypt(long long *message, unsigned long message_size, struct private_key_struct *priv){
   // We allocate space to do the decryption (temp) and space for the output as a char array
   char *decrypted = malloc(message_size/sizeof(long long));
   char *temp = malloc(message_size);
-  if((decrypted == NULL) || (temp == NULL)){
-    fprintf(stderr, "Error in decryption.\n");
-    return NULL;
-  }
+ 
   // Store each value of the decrypted message in temp, as a mediary for temp to decrypted
   long long i = 0;
   for(i=0; i < message_size/8; i++){
