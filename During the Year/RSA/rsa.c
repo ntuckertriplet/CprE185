a720b4a8e156df28502131d18065be667f7243ee
Nate Tucker <njtucker@iseage.org>
2019-04-30 21:44:43 -0500

    Cleanup

--- During the Year/RSA/rsa.c
+++ During the Year/RSA/rsa.c
@@ -22,18 +22,17 @@ struct private_key_struct{
 
 
 // Greatest common divisor
-long long gcd(long long a, long long b)
+long long gcd(long long x, long long y)
 {
-  long long c;
-  while ( a != 0 ) {
-    c = a; a = b%a;  b = c;
+  long long temp;
+  while ( x != 0 ) {
+    temp = x; x = y%x;  y = temp;
   }
-  return b;
+  return y;
 }
 
 
-long long calculate_d(long long a, long long b)
-{
+long long calculate_d(long long a, long long b){
  long long x = 0, d = 1, u = 1, v = 0, gcd = b, m, n, q, r;
  while (a!=0) {
    q = gcd/a; r = gcd % a;
@@ -43,25 +42,25 @@ long long calculate_d(long long a, long long b)
    return d;
 }
 
-long long rsa_modExp(long long b, long long e, long long m)
-{
-  if (b < 0 || e < 0 || m <= 0){
+// recursively go through and go from encrypted to decrypted or vice versa
+// the math works the same when given the public key to encrypt and the private key to decrypt
+long long calculate_mod_exp(long long mess, long long e, long long m){
+  if (mess < 0 || e < 0 || m <= 0){
     exit(1);
   }
-  b = b % m;
-  if(e == 0) return 1;
-  if(e == 1) return b;
-  if( e % 2 == 0){
-    return ( rsa_modExp(b * b % m, e/2, m) % m );
+  mess = mess % m;
+  if(e == 0) return 1; // x to the 0 is 1
+  if(e == 1) return b; // x to the 1 in x
+  if(e % 2 == 0){
+    return (calculate_mod_exp(mess * mess % m, e/2, m) % m);
   }
-  if( e % 2 == 1){
-    return ( b * rsa_modExp(b, (e-1), m) % m );
+  if(e % 2 == 1){
+    return (mess * calculate_mod_exp(mess, (e-1), m) % m);
   }
 
 }
 
-// Calling this function will generate a public and private key and store them in the pointers
-// it is given. 
+// generate a public and private key and store them in the pointers
 void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv, char *PRIME_SOURCE_FILE)
 {
   FILE *primes_list;
@@ -105,9 +104,6 @@ void rsa_gen_keys(struct public_key_struct *pub, struct private_key_struct *priv
     // here we find the prime at position a, store it as p
     rewind(primes_list);
     for(i=0; i < a + 1; i++){
-    //  for(j=0; j < MAX_DIGITS; j++){
-    //	prime_buffer[j] = 0;
-    //  }
       fgets(prime_buffer,sizeof(prime_buffer)-1, primes_list);
     }
     p = atol(prime_buffer); 
@@ -153,7 +149,7 @@ long long *rsa_encrypt(const char *message, const unsigned long message_size, co
   }
   long long i = 0;
   for(i=0; i < message_size; i++){
-    encrypted[i] = rsa_modExp(message[i], pub->exponent, pub->modulus);
+    encrypted[i] = calculate_mod_exp(message[i], pub->exponent, pub->modulus);
   }
   return encrypted;
 }
@@ -165,20 +161,18 @@ char *rsa_decrypt(const long long *message, const unsigned long message_size, co
      return NULL;
   }
   // We allocate space to do the decryption (temp) and space for the output as a char array
-  // (decrypted)
   char *decrypted = malloc(message_size/sizeof(long long));
   char *temp = malloc(message_size);
   if((decrypted == NULL) || (temp == NULL)){
     fprintf(stderr, "Error in decryption.\n");
     return NULL;
   }
-  // Now we go through each 8-byte chunk and decrypt it.
+  // Store each value of the decrypted message in temp, as a mediary for temp to decrypted
   long long i = 0;
   for(i=0; i < message_size/8; i++){
-    temp[i] = rsa_modExp(message[i], priv->exponent, priv->modulus);
+    temp[i] = calculate_mod_exp(message[i], priv->exponent, priv->modulus);
   }
-  // The result should be a number in the char range, which gives back the original byte.
-  // We put that into decrypted, then return.
+  // We move each character over now that it is in the range of char
   for(i=0; i < message_size/8; i++){
     decrypted[i] = temp[i];
   }
